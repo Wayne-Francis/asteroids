fbdb8944a88bf336cd3a20d626a4a6a87de12694
MrFaultyT <wanye787@gmail.com>
2025-05-27 13:58:04 +0100

    updated main file to open and close a black screen

--- main.py
+++ main.py
@@ -4,14 +4,22 @@
 
 import pygame
 
-from constants import (SCREEN_WIDTH, 
-                       SCREEN_HEIGHT, 
-                       ASTEROID_MIN_RADIUS, 
-                       ASTEROID_KINDS,
-                       ASTEROID_SPAWN_RATE,
-                       ASTEROID_MAX_RADIUS)
+from constants import * #(SCREEN_WIDTH, importing with wild car for now
+                       #SCREEN_HEIGHT, 
+                       #ASTEROID_MIN_RADIUS, 
+                       #ASTEROID_KINDS,
+                       #ASTEROID_SPAWN_RATE,
+                       #ASTEROID_MAX_RADIUS)
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while SCREEN_WIDTH == 1280:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
