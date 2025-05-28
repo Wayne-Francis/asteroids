4c8db1db840ea97327f8c5eb5912a74767cc9677
MrFaultyT <wanye787@gmail.com>
2025-05-28 20:02:44 +0100

    updated file to add objects

--- main.py
+++ main.py
@@ -13,24 +13,29 @@ from constants import * #(SCREEN_WIDTH, importing with wild car for now
                        #ASTEROID_SPAWN_RATE,
                        #ASTEROID_MAX_RADIUS)
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
 def main():
     pygame.init()
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while SCREEN_WIDTH == 1280:
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}") 
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60)/1000       
         screen.fill(color="black")
-        player.draw(screen)
-        player.update(dt) 
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}") 
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
