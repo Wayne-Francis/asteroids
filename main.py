9b1310624a8cd6cfce0c6acd2523cddd7efbdf26
MrFaultyT <wanye787@gmail.com>
2025-05-28 12:39:16 +0100

    Added a triangle to a black screen, updated main to call player and added 2 files - player and circleshape

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import pygame
 
+from player import *
+
 from constants import * #(SCREEN_WIDTH, importing with wild car for now
                        #SCREEN_HEIGHT, 
                        #ASTEROID_MIN_RADIUS, 
@@ -13,12 +15,17 @@ from constants import * #(SCREEN_WIDTH, importing with wild car for now
 
 def main():
     pygame.init()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while SCREEN_WIDTH == 1280:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000        
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}") 
